54cb8d76138674ac40c97bbb62e005695aeee6a3
tanyakalianda <42977166+tanyakalianda@users.noreply.github.com>
2018-09-26 13:39:09 -0400

    Add files via upload

---
+++ src/BankAccount.java
@@ -0,0 +1,23 @@
+
+public class BankAccount 
+{
+	private String name;
+	
+	private int accNum; 
+	
+	private double balance; 
+	
+	
+	public BankAccount(String n, int a)
+	{
+		name = n;
+		accNum = a;
+		balance = 0;
+	}
+	
+	public BankAccount(String name, accNum)
+	{
+		
+	}
+
+}
